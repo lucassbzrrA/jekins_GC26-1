011454604d3ce7a78ab79a95afa396ca41a36951
Lucas Bezerra <lucasbezerra2712@gmail.com>
2026-05-27 14:20:46 -0400

    Simulando erro de sintaxe

--- src/main/java/com/Conversor.java
+++ src/main/java/com/Conversor.java
@@ -2,5 +2,5 @@ package com;
 
 public class Conversor {
     public double cParaF(double c) { return (c * 1.8) + 32; }
-    public double fParaC(double f) { return (f - 32) / 1.8 }
+    public double fParaC(double f) { return (f - 32) / 1.8;}
 }
